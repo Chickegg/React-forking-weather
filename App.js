a48ee977e634d1625062a4e806e2c3c3a3af30b0
unknown <73797608+Chickegg@users.noreply.github.com>
2021-08-08 23:43:28 +0900

    Expo를 이용한 작업환경 구축

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello!!!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orangered',
     alignItems: 'center',
+    color: 'white',
     justifyContent: 'center',
   },
 });
